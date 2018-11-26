ff7082c5d8843dbe56ad3c7921e0e1f5c3d20377
BinarySoftware <mkmikolajek@icloud.com>
2018-11-26 17:14:52 +0100

    Terms WIP

---
+++ terms.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+  <title>StockExperience - Regulamin</title>
+  <?php include 'css/css.html'; ?>
+</head>
+<body>
+  <div class="form">
+          <table width=100%>
+            <tr>
+                <td width=110px><img src="icon.png"/ style="width:100px;height:100px;"></td>
+                <td><h1>Regulamin aplikacji StockExperience</h1></td> 
+                <td width=40% ><a href="index.php"><button class="button-logout" name="comeback"/>Powrót do strony głównej</button></a></td>
+            </tr>
+          </table>
+    <br />
+    <p>Work In Progress</p>
+    <br />
+  </div>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/index.js"></script>
+</body>
+</html>
