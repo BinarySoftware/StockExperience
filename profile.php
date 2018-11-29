d3c508de27645511c274691dce4b84328539cc30
BinarySoftware <mkmikolajek@icloud.com>
2018-11-29 23:30:16 +0100

    repair annoying html error, which actually doesnt do anything

--- profile.php
+++ profile.php
@@ -102,15 +102,18 @@ require 'IndexBackend.php';
     // Script for running the pop-up informing about buying/selling
     // Get the modal
     var modal = document.getElementById('myModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
     modal.style.display = "block";
 
-    // Get the <span> element that closes the modal. When the user clicks on <span> (x), close the modal
-    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
     }
 
-    // And when the user clicks anywhere outside of the modal, close it too
+    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
