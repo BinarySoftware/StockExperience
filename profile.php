b36013516f34ce99b87851a26917d8cb0ca2ef0e
BinarySoftware <mkmikolajek@icloud.com>
2018-12-07 19:36:54 +0100

    small change to fix one DOM error and style after removing iframe

--- profile.php
+++ profile.php
@@ -120,19 +120,19 @@ require 'backend/profileBackend.php';
     // Check if modal exists
     if (modal != null) {
       modal.style.display = "block";
-    }
+  
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+          modal.style.display = "none";
+      }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              modal.style.display = "none";
+          }
+      }
+   }
     </script>
     <script src="js/index.js"></script>
     <h5 style="color:rgba(19, 35, 47, 0.9)">StockExperience Ⓒ2018 BinarySoftware. Wszelkie prawa zastrzeżone.</h5>
