2fcbf1bfd8c910aa4737bddcdfafb9ed100d2e75
BinarySoftware <mkmikolajek@icloud.com>
2021-12-15 23:53:14 +0100

    xd

---
+++ backend/db.php
@@ -0,0 +1,12 @@
+<!--
+  db.html
+  StockExperience
+
+  Edited by BinarySoftware on 07/03/2019.
+  Copyright ©2019 BinarySoftware/Maciej Mikołajek. All rights reserved.
+
+  Purpose:  Database connection settings
+
+<?php
+
+?>
